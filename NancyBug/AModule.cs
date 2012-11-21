d113763bca482f32d9bc3db5af0966387e7011a1
Per Rovegard <per@rovegard.se>
2012-11-21 23:07:49 +0100

    all the stuff

---
+++ NancyBug/AModule.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Nancy;
+using Nancy.Responses;
+
+namespace NancyBug
+{
+    public class AModule : NancyModule
+    {
+        private readonly IRootPathProvider _rpp;
+
+        public AModule(IRootPathProvider rpp)
+        {
+            _rpp = rpp;
+            Get["/"] = _ => StreamResponse();
+        }
+
+        public Response StreamResponse()
+        {
+            return new StreamResponse(() => new FileStream(GetBigJsonPath(), FileMode.Open), "application/json");
+        }
+
+        private string GetBigJsonPath()
+        {
+            var path = "big.json";
+            if (!File.Exists(path))
+            {
+                path = _rpp.GetRootPath() + @"\bin\big.json";
+            }
+            return path;
+        }
+    }
+}
---
+++ Test/Tester.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Nancy.Testing;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tester
+    {
+        [Test]
+        public void ExposeTheBug()
+        {
+            var b = new Browser(c => { });
+            var response = b.Get("/");
+            var str = response.Body.AsString();
+            Assert.AreNotEqual("foo", str);
+        }
+    }
+}
